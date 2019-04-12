25dcbf786bd0af948d0ec2e3c5f480ee5dd16ee8
reecerussell <cobbles71@gmail.com>
2019-04-12 23:22:23 +0100

    changed debug error

--- Authentication/Controller.go
+++ Authentication/Controller.go
@@ -37,7 +37,7 @@ func (con *Controller) Login(w http.ResponseWriter, r *http.Request) {
 
 	token, err := con.Service.GetAuthToken(loginCredential.Email, loginCredential.Password)
 	if err != nil {
-		fmt.Println("\nError\n")
+		fmt.Println("    Error: " + err.Error() + "     ")
 		Helpers.Status500(w, err.Error())
 		return
 	}
